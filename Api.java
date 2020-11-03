3c61149ded1dd933b3360b47839157e534423466
asim altayb <asomy0909@gmail.com>
2020-11-03 18:48:53 +0200

    Create Api.java

---
+++ Api.java
@@ -0,0 +1,20 @@
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+ 
+
+ 
+public interface Api {
+ 
+    @POST("register.php")
+    @FormUrlEncoded
+    Call<model> register(@Field("username") String username, @Field("email") String email, @Field("password") String password);
+ 
+    @POST("login.php")
+    @FormUrlEncoded
+    Call<model> login(@Field("username") String username, @Field("password") String password);
+ 
+ 
+ 
+}
